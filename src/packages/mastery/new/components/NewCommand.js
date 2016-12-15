235d260ecb665e6eb1dd473aec002ebbfeff98c7
Muhammad Labib Ramadhan <mlabib.ramadhan@gmail.com>
2016-12-15 18:05:35 +0700

    Improve 'new' command

--- src/packages/mastery/new/components/NewCommand.js
+++ src/packages/mastery/new/components/NewCommand.js
@@ -1,10 +1,25 @@
+import _ from 'lodash';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
 import path from 'path';
 import simpleGit from 'simple-git';
 
 export default class NewCommand {
   execute = async (destination) => {
+    const resolvedDestination = path.resolve(destination);
+
+    console.log(chalk.yellow('Creating a new MasteryJS project'));
+
+    const questions = [{
+      name: 'name',
+      message: 'Project name:',
+      default: _.kebabCase(path.basename(resolvedDestination)),
+    }];
+
     const git = simpleGit();
     const repoUrl = conf.get('mastery:repoUrl');
-    await git.clone(repoUrl, path.resolve(destination));
+    await inquirer.prompt(questions);
+
+    await git.clone(repoUrl, path.resolve(resolvedDestination));
   }
 }
